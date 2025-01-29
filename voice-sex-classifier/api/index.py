e7caaf45dd10661d4732ce6e0c7b97ea6ff972d6
Vince Chapman <vchapandrews@gmail.com>
2025-01-29 22:04:35 +0000

    fix file name

--- voice-sex-classifier/api/index.py
+++ voice-sex-classifier/api/index.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Ensure we get the correct path
-MODEL_PATH = Path(__file__).parent / "svm.pkl"
+MODEL_PATH = Path(__file__).parent / "svm.plk"
 model = joblib.load(MODEL_PATH)  # Load model with preprocessing
 
 @app.route("/api/python")
